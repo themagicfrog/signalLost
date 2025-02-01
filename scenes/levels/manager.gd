7657b0999c5bb3957d42810b2f9f35cf1353fa95
estellagu <126267993+themagicfrog@users.noreply.github.com>
2025-01-31 22:40:19 -0500

    oxygen bubbles

--- scenes/levels/manager.gd
+++ scenes/levels/manager.gd
@@ -1,15 +1,19 @@
 extends Node
 
-@onready var coin_label: Label = %CoinLabel
+@onready var oxygenLevel: Label = %OxygenLabel
 @export var hearts : Array[Node]
+@onready var timer: Timer = $"../UI/Timer"
 
-var coins = 0
-var lives = 10
+var oxygen = 10
+var lives = 5
+
+func _ready():
+	timer.start()
+	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
 
 func lose_life():
 	lives -= 1
-	print(lives)
-	for h in 10:
+	for h in lives:
 		if (h < lives):
 			hearts[h].show()
 		else:
@@ -17,7 +21,13 @@ func lose_life():
 	if (lives == 0):
 		get_tree().reload_current_scene()
 
-func add_coin():
-	coins += 1
-	print (coins)
-	coin_label.text = str(coins)
+func addOxygen():
+	oxygen += 10
+	oxygenLevel.text = str(oxygen)
+	
+func _on_timer_timeout():
+	if oxygen > 0:
+		oxygen -= 1
+	oxygenLevel.text = str(oxygen)
+	if oxygen == 0:
+		lose_life()
--- scenes/objects/coin.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-@onready var manager: Node = %Manager
-
-func _on_body_entered(body: Node2D) -> void:
-	if (body.name == "Player"):
-		queue_free()
-		manager.add_coin()
